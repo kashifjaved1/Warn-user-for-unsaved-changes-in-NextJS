486f581c15f116d9e48224fa41346bc8b7ce2f76
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2022-08-01 23:40:25 +0500

    minor changes in scripts

--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -1,3 +1,2 @@
-/etc/init.d/apache2 start
 service apache2 start
-systemctl start apache2.service
+# systemctl start apache2.service
--- deployScripts/stop_server.sh
+++ deployScripts/stop_server.sh
@@ -1,3 +1,2 @@
-/etc/init.d/apache2 stop
 service apache2 stop
-systemctl stop apache2.service
+# systemctl stop apache2.service
