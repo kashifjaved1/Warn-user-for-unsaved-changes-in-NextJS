560ded0264e73715ab0e0b1f219948fdd6f7f097
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2022-08-02 21:33:59 +0500

    ..

--- deployScripts/setup_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /var/www/html/Warn-user-for-unsaved-changes-in-NextJS/
-# npm install
-npm install
--- deployScripts/start_app.sh
+++ deployScripts/start_app.sh
@@ -1,3 +1,4 @@
 cd /var/www/html/Warn-user-for-unsaved-changes-in-NextJS/
+npm install
 npm run build
 npm start
