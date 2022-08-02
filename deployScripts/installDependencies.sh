eacba85a6b9429923766cc33a79595cde0e85fc5
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2022-08-02 13:42:07 +0500

    .

--- deployScripts/installDependencies.sh
+++ deployScripts/installDependencies.sh
@@ -3,7 +3,5 @@ apt install -y apache2
 apt install -y nodejs
 apt install -y npm
 npm install
-service codedeploy-agent stop
-rm -rf /opt/codedeploy-agent/deployment-root/*
-service codedeploy-agent start
+service codedeploy-agent restart
 rm -rf /var/www/html/*
