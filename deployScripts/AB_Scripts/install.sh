0e249223d892386f13fad95dcb2cacf46737f39f
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2022-08-19 00:00:50 +0500

    Update install.sh

--- deployScripts/AB_Scripts/install.sh
+++ deployScripts/AB_Scripts/install.sh
@@ -4,7 +4,12 @@ set -e
 # update instance
 apt -y update
 
+#install nodejs
+apt install nodejs -y
+node -v
+apt install npm -y
+
 # install pm2 module globaly
 npm install -g pm2
-pm2 update
+# pm2 update
 rm -rf ~/node
