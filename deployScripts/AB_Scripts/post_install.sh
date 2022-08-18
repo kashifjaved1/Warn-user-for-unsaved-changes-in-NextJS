ed2286395c8da056d35be0623a07b8b90c5929c0
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2022-08-19 01:08:55 +0500

    Update post_install.sh

--- deployScripts/AB_Scripts/post_install.sh
+++ deployScripts/AB_Scripts/post_install.sh
@@ -2,5 +2,14 @@
 set -e
 
 cd /home/ubuntu/node/
+
+# updating nodejs
+node -v
+npm install -g n
+n lts
+n prune
+node -v
+
+# installing project dependencies and building project
 npm install
 npm run build
